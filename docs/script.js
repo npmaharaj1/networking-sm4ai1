94f32be355f1e250f66c49333c4c75f9a0ceb9e4
Nishant Maharaj <npmaharaj1@protonmail.com>
2025-07-30 19:36:32 +1000

    Replaced mp4 with gif for better browser compatability

--- docs/script.js
+++ docs/script.js
@@ -1,16 +1,14 @@
 "use strict";
 
 const label = document.getElementById("label");
-const video = document.getElementById("video");
+const video = document.getElementById("gif");
 
 const fadeInAnim = [
   { filter: "drop-shadow(-4vw 0vh 1vh) drop-shadow(4vw 0vh 1vh) blur(1vh)", opacity: "0%" },
   { filter: "drop-shadow(0vw 0vh 0vh) drop-shadow(0vw 0vh 0vh)", opacity: "100%" }
 ];
 
-video.play().then(() =>
-  setTimeout(() => {
+setTimeout(() => {
     label.animate(fadeInAnim, 1000);
     label.removeAttribute("style");
-  }, 8291)
-);
+}, 8291)
