a2178162d6592bd007b5051e740e6b0ede739e9b
nothing1649 <65153224+nothing1649@users.noreply.github.com>
2025-07-30 17:08:58 +1000

    start wait timer once the video actually starts playing

--- docs/script.js
+++ docs/script.js
@@ -5,11 +5,12 @@ const video = document.getElementById("video");
 
 const fadeInAnim = [
   { filter: "drop-shadow(-4vw 0vh 1vh) drop-shadow(4vw 0vh 1vh) blur(15px)", opacity: "0%" },
-  { filter: "drop-shadow(0vw 0vh 0vh) drop-shadow(0vw 0vh 0vh)", opacity: "100%" },
+  { filter: "drop-shadow(0vw 0vh 0vh) drop-shadow(0vw 0vh 0vh)", opacity: "100%" }
 ];
 
-video.play();
-setTimeout(() => {
-  label.animate(fadeInAnim, { duration: 1000 });
-  label.removeAttribute("style");
-}, 8291);
+video.play().then(() =>
+  setTimeout(() => {
+    label.animate(fadeInAnim, 1000);
+    label.removeAttribute("style");
+  }, 8291)
+);
