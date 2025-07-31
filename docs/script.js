7b50b6dddf6f1f46cd242af0ba4aab86a4139c45
Nishant Maharaj <npmaharaj1@protonmail.com>
2025-08-01 09:44:13 +1000

    fix sync issues on safari

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,14 @@
-const label = document.getElementById("label");
-const video = document.getElementById("gif");
+function labelInit() {
+    const label = document.getElementById("label");
+    const video = document.getElementById("gif");
 
-const fadeInAnim = [
-  { filter: "drop-shadow(-4vw 0vh 1vh) drop-shadow(4vw 0vh 1vh) blur(1vh)", opacity: "0%" },
-  { filter: "drop-shadow(0vw 0vh 0vh) drop-shadow(0vw 0vh 0vh)", opacity: "100%" }
-];
+    const fadeInAnim = [
+        { filter: "drop-shadow(-4vw 0vh 1vh) drop-shadow(4vw 0vh 1vh) blur(1vh)", opacity: "0%" },
+        { filter: "drop-shadow(0vw 0vh 0vh) drop-shadow(0vw 0vh 0vh)", opacity: "100%" }
+    ];
 
-setTimeout(() => {
-    label.animate(fadeInAnim, 1000);
-    label.removeAttribute("style");
-}, 8291)
+    setTimeout(() => {
+        label.animate(fadeInAnim, 1000);
+        label.removeAttribute("style");
+    }, 8291)
+}
