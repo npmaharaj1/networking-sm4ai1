a5ea28dbfd6680b3d5472567b5e3f1f67437f2a4
nothing1649 <65153224+nothing1649@users.noreply.github.com>
2025-08-05 11:11:43 +1000

    add clang-format and reformat bomb.c; make functions static

--- dummydevice/bomb.c
+++ dummydevice/bomb.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 
-void startAgain() {
+static void startAgain() {
     printf("\nIncorrect Password Entered.\n");
     printf("- DATA DELETION IN -\n");
     sleep(1);
@@ -21,7 +21,7 @@ void startAgain() {
     // Kill the process here and make players start from beginning of ctf
 }
 
-void reroutePlayer() {
+static void reroutePlayer() {
     printf("\n...\n");
     sleep(2);
     printf("...\n");
@@ -35,7 +35,10 @@ void reroutePlayer() {
     sleep(2);
     printf("I hate to break it to you, but this isn't the real bomb.\n");
     sleep(3);
-    printf("Sending you on this goose chase provided me with great information about you and your 'team'.\n");
+    printf(
+        "Sending you on this goose chase provided me with great information about you and your "
+        "'team'.\n"
+    );
     sleep(3);
     printf("It's a shame really, I was thinking about postponing the deletion..\n");
     sleep(3);
@@ -64,9 +67,13 @@ int main() {
 
     printf("Hello defender, it looks like you have found me.\n");
     sleep(4);
-    printf("What you are looking at right now (this file) is the bomb responsible for mass data deletion.\n");
+    printf(
+        "What you are looking at right now (this file) is the bomb responsible for mass data "
+        "deletion.\n"
+    );
 
-    char name[101] = "The tears of a clown make my lipstick run, but my shower cap is still intact.\n";
+    char name[101] =
+        "The tears of a clown make my lipstick run, but my shower cap is still intact.\n";
 
     sleep(4);
     printf("If you fail to defuse this, you will be held responsible for everyone's data loss.\n");
@@ -77,7 +84,7 @@ int main() {
 
     char answer[101];
     fgets(answer, 100, stdin);
-    
+
     if (!strcmp(name, answer)) {
         reroutePlayer();
     } else {
