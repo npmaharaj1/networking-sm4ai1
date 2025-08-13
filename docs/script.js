fe3a2f92d5a6ffba2fd47fb92240e6d0c5d499b4
Nishant Maharaj <npmaharaj1@protonmail.com>
2025-08-13 17:20:06 +1000

    warning message with countdown

--- docs/script.js
+++ docs/script.js
@@ -66,3 +66,27 @@ function labelInit() {
 
 gif.onload = labelInit;
 gif.src = "assets/video.gif";
+
+var countDownDate = new Date("Jan 5, 2030 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+    
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Challenge Complete, thanks for playing!";
+  }
+}, 1000);
