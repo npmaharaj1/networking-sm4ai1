06c53bea29b60514b1d0987dcd304a23fd7b167f
Nishant <109418510+npmaharaj1@users.noreply.github.com>
2025-08-20 07:34:58 +0000

    add not-target

--- start.sh
+++ start.sh
@@ -2,25 +2,7 @@
 
 cd vm/
 if [[ "$1" == "deploy" ]]; then
-    export COUNT=254
-    while (( $COUNT > 1 )); do
-        response=$(sudo arp-scan 10.1.1.$COUNT | grep responded | awk '{print $(NF-1)}')
-        if [[ $response == '0' ]]; then
-
-            echo 'Using $COUNT'
-
-            export PRECOUNT=10.0.0
-
-            sudo ip link add sm4ai1_bridge type bridge
-            sudo ip addr add $PRECOUNT.$COUNT dev sm4ai1_bridge
-
-            sudo PRECOUNT=$PRECOUNT ADDRESS=$COUNT bash -c "docker compose up -d --build"
-
-            exit
-        fi
-        : $((COUNT--))
-    done
-
+    sudo docker compose up -d --build
 elif [[ "$1" == "destroy" ]]; then
     sudo ip link delete sm4ai1_bridge
     sudo docker rm -f $(sudo docker ps -aq) # Will error out here if not using
@@ -28,5 +10,3 @@ elif [[ "$1" == "destroy" ]]; then
 else
     echo "Use ./start.sh deploy|destroy"
 fi
-
-
