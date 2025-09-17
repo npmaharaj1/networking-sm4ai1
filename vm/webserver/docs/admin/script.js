470d9edc4787a15f254a64155d2aa14a044a0364
Nishant Maharaj <npmaharaj1@protonmail.com>
2025-09-17 14:13:08 +1000

    target cli

--- vm/webserver/docs/admin/script.js
+++ vm/webserver/docs/admin/script.js
@@ -1,4 +1,4 @@
-document.getElementById("terminaliframe").src = location.host;
+document.getElementById("terminaliframe").src = "http://" + location.host + ":8080";
 
 if (localStorage.getItem("SessionID") != "5169bc07cec55c46d3fc619eee92ccf7") {
     window.location.replace("./login");
