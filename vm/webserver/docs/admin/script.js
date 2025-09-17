98783a5538dee4e7ef325fdd4422c58a6e1abb0f
Nishant Maharaj <npmaharaj1@protonmail.com>
2025-09-17 14:09:35 +1000

    get url

--- vm/webserver/docs/admin/script.js
+++ vm/webserver/docs/admin/script.js
@@ -1,3 +1,5 @@
+document.getElementById("terminaliframe").src = location.host;
+
 if (localStorage.getItem("SessionID") != "5169bc07cec55c46d3fc619eee92ccf7") {
     window.location.replace("./login");
 }
